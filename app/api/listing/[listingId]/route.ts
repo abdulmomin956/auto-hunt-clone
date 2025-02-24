2a4543f18ffced583c74586e2148a3e0d876012c
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2025-02-24 23:27:08 +0600

    first commit

---
+++ app/api/listing/[listingId]/route.ts
@@ -0,0 +1,34 @@
+import { APP_CONFIG } from "@/lib/app-config";
+import { createAnonymousClient } from "@/lib/appwrite";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (
+    req: NextRequest,
+    { params }: { params: Promise<{ listingId: string }> }
+) => {
+    try {
+        //console.log('params=>', await params)
+        const { listingId } = await params;
+        const { databases } = await createAnonymousClient();
+
+        const listing = await databases.getDocument(
+            APP_CONFIG.APPWRITE.DATABASE_ID,
+            APP_CONFIG.APPWRITE.CAR_LISTING_ID,
+            listingId
+        );
+        return NextResponse.json(
+            {
+                message: "Listing fetched successfully",
+                listing,
+            },
+            { status: 200 }
+        );
+    } catch (error: any) {
+        return NextResponse.json(
+            {
+                error: error?.message || "Internal Server Error",
+            },
+            { status: 500 }
+        );
+    }
+};
